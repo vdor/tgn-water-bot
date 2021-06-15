dcb821a6a457756be76fab3d9f9a91faa0a73856
Vseslav Dorofeev <vseslav.dorofeev@gmail.com>
2021-06-15 19:15:03 +0300

    feat: create script to print issues

--- README.md
+++ README.md
@@ -8,6 +8,25 @@ Telegram bot -  [@TgnWaterBot](https://t.me/TgnWaterBot)
 
 Telegram [channel](https://t.me/tgnwater) with the bot
 
-### Usage
+## Usage
 
 Send `/start` message to the bot, use it as administrator in your channel or add to a group
+
+## Source code usage
+
+- create venv `virtual venv -p $(which python3.9)`
+- activate venv `source venv/bin/activate`
+
+### Display issues to the console
+
+`python3 display_issues.py`
+
+### Run bot
+
+- create `.env` based on `.env.example`
+- fill in `.env`
+- `python3 worker.py`
+
+## TODO
+
+- Create `manage.py` helper
---
+++ display_issues.py
@@ -0,0 +1,32 @@
+import asyncio
+import subprocess
+import sys
+import textwrap
+
+from core.env import URI_WATER_ISSUES_SOURCE_HTML
+from parsers.issues_parser.html import IssuesParserHTML
+from repositories.issues_html_repository.http import IssuesHTMLRepositoryHTTP
+
+if __name__ == "__main__":
+    issues_html_repo = IssuesHTMLRepositoryHTTP(uri=URI_WATER_ISSUES_SOURCE_HTML)
+    issues_html_parser = IssuesParserHTML(repo=issues_html_repo)
+
+    issues = asyncio.run(issues_html_parser.parse())
+
+    try:
+        pager = subprocess.Popen(
+            ["less", "-F", "-R", "-S", "-X", "-K"],
+            stdin=subprocess.PIPE,
+            stdout=sys.stdout,
+        )
+        for issue in issues:
+
+            for line in textwrap.wrap(issue.formatted):
+                pager.stdin.write((line + "\n").encode())
+
+            pager.stdin.write("\n------\n".encode())
+
+        pager.stdin.close()
+        pager.wait()
+    except KeyboardInterrupt:
+        pass
